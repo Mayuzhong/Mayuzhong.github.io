363c503e8f6b79cb071d0e3ebbf35193182b861e
mayuzhong <mayuzhong@kanzhun.com>
2018-08-05 17:28:54 +0800

    马玉忠first

---
+++ mayuzhong/src/packageName/class_1.java
@@ -0,0 +1,13 @@
+package packageName;
+
+/**
+ * @author mayuzhong
+ * @date 2018/8/5
+ * @describe
+ */
+public class class_1 {
+
+    public static void main(String[] args) {
+        System.out.println("马玉忠");
+    }
+}
